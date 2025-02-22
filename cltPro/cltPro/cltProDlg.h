dbe6ee27dd107f4e0f25f336d652c3e0b08a2767
jihu1130 <163612182+jihu1130@users.noreply.github.com>
2025-02-22 13:28:35 +0900

    Add files via upload

---
+++ cltPro/cltPro/cltProDlg.h
@@ -0,0 +1,38 @@
+﻿
+// cltProDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CcltProDlg 대화 상자
+class CcltProDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CcltProDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CLTPRO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CProgressCtrl m_ctlPro;
+	afx_msg void OnButtExit();
+	afx_msg void OnButtStart();
+};
