3e872d85e1163eda466cc3fe826bf8d776b46699
jihu1130 <163612182+jihu1130@users.noreply.github.com>
2025-02-22 13:37:06 +0900

    Add files via upload

---
+++ ctlCheck/ctlCheck/ctlCheckDlg.h
@@ -0,0 +1,39 @@
+﻿
+// ctlCheckDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CctlCheckDlg 대화 상자
+class CctlCheckDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CctlCheckDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CTLCHECK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strEdit;
+	BOOL m_ctlCheck;
+	afx_msg void OnCheckMess();
+	afx_msg void OnButtExit();
+};
